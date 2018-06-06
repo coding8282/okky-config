449e822e4723bf9619c45357318a079ec69edea8
hyunsu <coding8282@gmail.com>
2018-06-06 20:56:10 +0900

    배포스크립트 누락된 키값을 파라미터스토어에서 읽어오도록 수정

--- integration/scripts/start-app.sh
+++ integration/scripts/start-app.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+git_uri=$(aws ssm get-parameter --name /okky/okky-config/git_uri --region ap-northeast-2 --with-decryption --query Parameter.Value --output text)
+git_username=$(aws ssm get-parameter --name /okky/okky-config/git_username --region ap-northeast-2 --with-decryption --query Parameter.Value --output text)
+git_password=$(aws ssm get-parameter --name /okky/okky-config/git_password --region ap-northeast-2 --with-decryption --query Parameter.Value --output text)
+
 nohup java -jar \
 /home/ec2-user/okky-config-1.0.0.jar \
 --spring.cloud.config.server.git.uri=$git_uri \
