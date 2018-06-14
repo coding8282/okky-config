c9c085664d2de605ae9ed9904416e7dee8ebbe5a
hyunsu <coding8282@gmail.com>
2018-06-14 21:08:46 +0900

    환경설정을 독립하면서 불필요한 내용 삭제

--- integration/scripts/start-app.sh
+++ integration/scripts/start-app.sh
@@ -9,4 +9,4 @@ nohup java -jar \
 --spring.cloud.config.server.git.uri="$git_uri" \
 --spring.cloud.config.server.git.username="$git_username" \
 --spring.cloud.config.server.git.password="$git_password" \
---spring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &
+> /dev/null 2> /dev/null < /dev/null &
